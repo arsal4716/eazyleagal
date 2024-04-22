c74703149f6b62e770e1d0318ef3f85421133545
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-22 21:52:27 +0500

    fix files

--- modal/orders.js
+++ modal/orders.js
@@ -90,6 +90,7 @@ const orderSchema = new mongoose.Schema({
   },
 });
 
+
 const Order = mongoose.model('Order', orderSchema);
 
 module.exports = Order;
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 app.get('*', (req, res) => {
-    app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
