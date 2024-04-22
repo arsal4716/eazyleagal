246e1de92bd073bd0480e568ebdcf89c253e2e69
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-22 18:55:24 +0500

    fix path issue

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json());
 app.use('/', submit);
 app.use('/', portal);
 app.use('/',d1);
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
