7e4008d4ae4e4ee9c029ba1940bf9355f6b6a650
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-22 22:06:00 +0500

    fix some files path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('frontend/build'));
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
