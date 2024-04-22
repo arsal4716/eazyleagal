2f33aab8d3374b912aa7a5e7ec9e80dfbe0b3b50
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-22 18:46:34 +0500

    add db

--- db/conn.js
+++ db/conn.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); // Load environment variables from .env
 
-const db = mongoose.connect('mongodb://localhost:27017/accident')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
-// const db = 'mongodb+srv://haristariq894:EIh2X5iJqy4Rl2X5@cluster0.kv9u3su.mongodb.net/getAccident';
-// try {
-//     mongoose.connect(db);
-//     const dbConnection = mongoose.connection;
+// const db = mongoose.connect('mongodb://localhost:27017/accident')
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((error) => console.error('Error connecting to MongoDB:', error));
+const db = 'mongodb+srv://haristariq894:EIh2X5iJqy4Rl2X5@cluster0.kv9u3su.mongodb.net/getAccident';
+try {
+    mongoose.connect(db);
+    const dbConnection = mongoose.connection;
   
-//     dbConnection.once('open', () => {
-//       console.log('Connected to MongoDB');
-//     });
+    dbConnection.once('open', () => {
+      console.log('Connected to MongoDB');
+    });
   
-//     dbConnection.on('error', (error) => {
-//       console.error('Error connecting to MongoDB:', error.message);
-//     });
-//   } catch (error) {
-//     console.error('Error in MongoDB connection setup:', error.message);
-//   }
+    dbConnection.on('error', (error) => {
+      console.error('Error connecting to MongoDB:', error.message);
+    });
+  } catch (error) {
+    console.error('Error in MongoDB connection setup:', error.message);
+  }
 
 module.exports = db;
