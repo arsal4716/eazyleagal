1e5aae76e47ca4fd0dc15a0dff966bbab7b2f881
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-22 18:52:07 +0500

    fix file issue

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/',d1);
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
