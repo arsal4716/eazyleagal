65bf32861a8913c7017e79d3fcdb0ea19107c89a
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-22 23:58:03 +0500

    fix issue

--- routes/portal.js
+++ routes/portal.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Order = require('../modal/orders'); // Assuming you have a model defined for your orders
+const Order = require('../modal/orders'); 
 
 router.get('/portal', async (req, res) => {
   try {
-    // Fetch data from the database
     const orders = await Order.find();
     res.json(orders);
+    console.log(orders)
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ message: 'Internal server error' });
--- server.js
+++ server.js
@@ -14,14 +14,16 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', submit);
-app.use('/', portal);
-app.use('/',d1);
 app.use(express.static('frontend/build'));
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend','build', 'index.html'));
 });
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+app.use('/', submit);
+app.use('/', portal);
+app.use('/',d1);
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
