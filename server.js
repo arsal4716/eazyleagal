3deb2beb2b3461204e7baf5c933303cbfc7aa051
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-22 22:01:30 +0500

    fix path issue

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.use(express.static('frontend/build'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
